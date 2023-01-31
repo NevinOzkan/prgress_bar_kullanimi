2e876a0dd5da192b2e484986692e41c4e1543d95
NevinOzkan <ozkannevin73@gmail.com>
2023-01-31 19:06:36 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/prgress_bar_kullanimi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.prgress_bar_kullanimi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool progressBarGorun = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Visibility(
+              visible: progressBarGorun,
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.deepPurple),
+              ),
+            ),
+            ElevatedButton(
+              child: Text("Başla"),
+              onPressed: () {
+                setState(() {
+                  progressBarGorun = true;
+                });
+              },
+            ),
+            ElevatedButton(
+              child: Text("Dur"),
+              onPressed: () {
+                setState(() {
+                  progressBarGorun = false;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
